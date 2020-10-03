1b3f21e71f38e05f2f387f518826b648dd43ff4f
Megan-Morales <63917946+Megan-Morales@users.noreply.github.com>
2020-10-03 08:39:19 -0600

    Diseño form principal 1.1

--- Proyecto1_MeganMorales_1221120_0.1/MyForm.h
+++ Proyecto1_MeganMorales_1221120_0.1/MyForm.h
@@ -43,6 +43,8 @@ namespace Proyecto1MeganMorales122112001 {
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button5;
 
 	protected:
 
@@ -75,6 +77,8 @@ namespace Proyecto1MeganMorales122112001 {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -84,7 +88,7 @@ namespace Proyecto1MeganMorales122112001 {
 			this->label1->BackColor = System::Drawing::SystemColors::HighlightText;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(143, 37);
+			this->label1->Location = System::Drawing::Point(354, 23);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(424, 32);
 			this->label1->TabIndex = 0;
@@ -93,9 +97,9 @@ namespace Proyecto1MeganMorales122112001 {
 			// pictureBox1
 			// 
 			this->pictureBox1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.BackgroundImage")));
-			this->pictureBox1->Location = System::Drawing::Point(78, 228);
+			this->pictureBox1->Location = System::Drawing::Point(12, 100);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(227, 176);
+			this->pictureBox1->Size = System::Drawing::Size(587, 646);
 			this->pictureBox1->TabIndex = 1;
 			this->pictureBox1->TabStop = false;
 			// 
@@ -105,7 +109,7 @@ namespace Proyecto1MeganMorales122112001 {
 			this->label2->BackColor = System::Drawing::Color::White;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(417, 113);
+			this->label2->Location = System::Drawing::Point(688, 100);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(284, 20);
 			this->label2->TabIndex = 2;
@@ -115,11 +119,11 @@ namespace Proyecto1MeganMorales122112001 {
 			// 
 			this->button1->BackColor = System::Drawing::Color::White;
 			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
-			this->button1->Location = System::Drawing::Point(496, 145);
+			this->button1->Location = System::Drawing::Point(798, 133);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(147, 36);
 			this->button1->TabIndex = 3;
-			this->button1->Text = L"INICIAR JUEGO";
+			this->button1->Text = L"MODO FÁCIL";
 			this->button1->UseVisualStyleBackColor = false;
 			// 
 			// label3
@@ -128,7 +132,7 @@ namespace Proyecto1MeganMorales122112001 {
 			this->label3->BackColor = System::Drawing::Color::White;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(417, 260);
+			this->label3->Location = System::Drawing::Point(688, 365);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(331, 20);
 			this->label3->TabIndex = 6;
@@ -138,7 +142,7 @@ namespace Proyecto1MeganMorales122112001 {
 			// 
 			this->button2->BackColor = System::Drawing::Color::White;
 			this->button2->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
-			this->button2->Location = System::Drawing::Point(496, 309);
+			this->button2->Location = System::Drawing::Point(798, 397);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(147, 36);
 			this->button2->TabIndex = 7;
@@ -151,29 +155,53 @@ namespace Proyecto1MeganMorales122112001 {
 			this->label4->BackColor = System::Drawing::Color::White;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label4->Location = System::Drawing::Point(417, 411);
+			this->label4->Location = System::Drawing::Point(692, 540);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(280, 20);
 			this->label4->TabIndex = 8;
-			this->label4->Text = L"2. Para salir del juego, presione aquí";
+			this->label4->Text = L"3. Para salir del juego, presione aquí";
 			// 
 			// button3
 			// 
 			this->button3->BackColor = System::Drawing::Color::White;
 			this->button3->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
-			this->button3->Location = System::Drawing::Point(496, 466);
+			this->button3->Location = System::Drawing::Point(798, 590);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(147, 36);
 			this->button3->TabIndex = 9;
 			this->button3->Text = L"SALIDA";
 			this->button3->UseVisualStyleBackColor = false;
 			// 
+			// button4
+			// 
+			this->button4->BackColor = System::Drawing::Color::White;
+			this->button4->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->button4->Location = System::Drawing::Point(798, 249);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(147, 36);
+			this->button4->TabIndex = 10;
+			this->button4->Text = L"MODO DIFÍCIL";
+			this->button4->UseVisualStyleBackColor = false;
+			// 
+			// button5
+			// 
+			this->button5->BackColor = System::Drawing::Color::White;
+			this->button5->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->button5->Location = System::Drawing::Point(798, 193);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(147, 36);
+			this->button5->TabIndex = 11;
+			this->button5->Text = L"MODO MEDIO";
+			this->button5->UseVisualStyleBackColor = false;
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
-			this->ClientSize = System::Drawing::Size(793, 610);
+			this->ClientSize = System::Drawing::Size(1054, 758);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->button2);
